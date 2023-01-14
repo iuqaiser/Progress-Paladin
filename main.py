90a2d4ba83d5d06e055d18e902b66c399c5245ea
Iman Umair-Qaiser <72308006+iuqaiser@users.noreply.github.com>
2023-01-14 20:57:53 +0000

    add flask

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask
+app = Flask(  # Create a flask app
+	__name__,
+	template_folder='templates',  # Name of html file folder
+	static_folder='static'  # Name of directory for static files
+)
 
-app = Flask(__name__)
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+	)
 
-@app.route("/")
-def index():
-    return "Congratulations, it's a web app!"
+@app.route('/')  # '/' for the default page
+def home():
+	return "Wow this is a basic output!"
+
